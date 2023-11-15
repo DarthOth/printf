8205a8b2ae9b61a5730b498e83dc18614de2bcf4
Othmane <chahnaouiothmane.2002@gmail.com>
2023-11-14 18:12:34 -0800

    task 0

--- main.h
+++ main.h
@@ -3,7 +3,16 @@
 
 #include <stdio.h>
 
+#define BUFF_SIZE 1024
 int _printf(const char *format, ...);
 void print_buffer(char buffer[], int *buff_count);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
+
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+
 
 #endif
